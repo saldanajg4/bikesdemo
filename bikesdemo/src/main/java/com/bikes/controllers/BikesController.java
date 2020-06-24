5bdeefb59b418265795b064cb8ed392a79b7cee4
Jose Gabriel <saldanajg4@hotmail.com>
2020-06-24 14:13:25 -0500

    Accessing the bike repository to find All, get one by id, and saving into bike table using save jpa command.

--- bikesdemo/src/main/java/com/bikes/controllers/BikesController.java
+++ bikesdemo/src/main/java/com/bikes/controllers/BikesController.java
@@ -2,9 +2,12 @@ package com.bikes.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.bikes.models.Bike;
+import com.bikes.repositories.BikeRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,20 +20,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/bikes")
 @RestController
 public class BikesController {
-    @GetMapping
-    public List<Bike> list(){
-        List<Bike> bikes = new ArrayList<>();
-        return bikes;
+    @Autowired
+    private BikeRepository bikeRepository;
+
+    public BikesController(BikeRepository bikeRepository) {
+        this.bikeRepository = bikeRepository;
     }
 
+    @GetMapping
+    public List<Bike> list(){
+        var allBikes = this.bikeRepository.findAll();
+        return allBikes;
+    }
+    
+    /**
+     * Take the bike body and persist it using Jpa save method to insert it 
+     * into db.
+     * @param bike
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Bike bike){
-
+        this.bikeRepository.save(bike);
     }
 
     @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id){
-        return new Bike();
+        Bike bike = this.bikeRepository.getOne(id);
+
+        return bike;
     }
+
+
 }
--- bikesdemo/src/main/java/com/bikes/repositories/BikeRepository.java
+++ bikesdemo/src/main/java/com/bikes/repositories/BikeRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 /***
  * instead of using generict types, use Dojo entities like bike and 
- * Long for our primary key Id
+ * Long for our primary key Id.
+ * It can create, select, update, delete CRUD and no code needed
  */
 public interface BikeRepository extends JpaRepository<Bike, Long> {
     
