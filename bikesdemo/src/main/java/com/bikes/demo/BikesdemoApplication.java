38c52c4058dfdcc55ee85fb178e1a55f7be85b51
Jose Gabriel <saldanajg4@hotmail.com>
2020-06-24 09:39:36 -0500

    Added component scan to theroot of project so it can find endpoints. Sqlite3 set in environment variables.  Created tables using sqlite3 from the vscode terminal project.

--- bikesdemo/src/main/java/com/bikes/demo/BikesdemoApplication.java
+++ bikesdemo/src/main/java/com/bikes/demo/BikesdemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/***
+ * very important to add Component scan so it can find the right classpath
+ */
 @SpringBootApplication
 @ComponentScan(basePackages={"com.bikes"})
 public class BikesdemoApplication {
