3c89f5981bc441f0b8bb0693d292f3ffc5f22fdb
Jose Gabriel <saldanajg4@hotmail.com>
2020-06-24 08:04:30 -0500

    first apis endpoints configuration accessible and tested using Postman.

--- bikesdemo/src/main/java/com/bikes/demo/BikesdemoApplication.java
+++ bikesdemo/src/main/java/com/bikes/demo/BikesdemoApplication.java
@@ -2,8 +2,10 @@ package com.bikes.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.bikes"})
 public class BikesdemoApplication {
 
 	public static void main(String[] args) {
