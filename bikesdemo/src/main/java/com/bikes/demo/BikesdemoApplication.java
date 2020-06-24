8a0e13dd5139bcccf657270c954f8760ff6b263f
Jose Gabriel <saldanajg4@hotmail.com>
2020-06-23 20:58:24 -0500

    Spring boot initializr

---
+++ bikesdemo/src/main/java/com/bikes/demo/BikesdemoApplication.java
@@ -0,0 +1,13 @@
+package com.bikes.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikesdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikesdemoApplication.class, args);
+	}
+
+}
---
+++ bikesdemo/src/test/java/com/bikes/demo/BikesdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
